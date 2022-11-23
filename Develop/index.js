885dc17999c91ab7da86566cfcef07c887aa236f
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-11-22 20:09:26 -0800

    Add template for README.md file text content. Traverse responses object to extract data. Data rendered to README.md file through template literals.

--- Develop/index.js
+++ Develop/index.js
@@ -53,4 +53,58 @@ const questions = [{
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) { }
-inquirer.prompt(questions).then((data) => console.log(data));
+inquirer.prompt(questions).then((data) => {
+    const template = `
+# ${data.title.trim()}
+![GitHub license](https://img.shields.io/badge/license-${data.license.replace(" ","%20")}-blue.svg)
+
+## Description
+${data.description.trim()}
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation.trim()}
+\`\`\`
+
+## Usage
+${data.usage.trim()}
+
+## License
+This project is licensed under the ${data.license.trim()} license.
+
+## Contributing
+${data.contribution.trim()}
+
+## Tests
+To run tests, run the following command:
+\`\`\`
+${data.tests.trim()}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email.trim()}. You can find more of my work at [${data.username.trim()}](https://github.com/${data.username.trim()}).
+`
+
+    fs.writeFile("./README.md", template, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+    console.log(data);
+});
